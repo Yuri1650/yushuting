1777dd31498d1afda21bb0aa580d2495d1b0a207
Yuri <1065029883@qq.com>
2020-12-04 12:08:57 +0800

    第一次向github推送代码

---
+++ demo01.py
@@ -0,0 +1,38 @@
+"""
+print("你好！")    #字符串
+print(2333)        #整数
+print(2.33)       #小数
+print(True)      #布尔值
+print(())       #元组
+print([])       #数组
+print({})       #字典
+print("哈哈"+"嘻嘻")   #字符串的拼接
+print("哈哈哈"*100)
+print(2>3)
+"""
+
+"""
+a = input("请输入：")
+print("input获取的内容:",a)
+"""
+
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容:",a+b)
+
+# 数据格式的转换
+# print(type("哈哈"))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+a = "1234567jhgfd gfdsgsdgfgfdfs  ssf"
+b = "453 hshgfh  dfh gdh hd "
+print(a)
+print(b)
+print(len(a)+len(b))
